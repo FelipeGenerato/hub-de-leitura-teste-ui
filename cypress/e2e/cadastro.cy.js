30cb55bae734df6aa02dd0dad7b929dbc13c5bfb
Felipe Generato <felipe.g.goncalves@hotmail.com>
2026-05-17 18:36:01 -0300

    Automacao do Contato

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,11 @@
+describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
+
+    beforeEach(() => {
+        cy.visit('register.html')
+    });
+
+    it('Deve fazer cadastro com sucesso', () => {
+        
+    });
+    
+});
